2d9b3d080d679dd97b9f5d5a91a5b7bb7f847704
lbferrand <lbferrand@gmail.com>
2022-05-01 20:55:49 -0400

    Add main content to html and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ const openMenuIcon = document.getElementById("menu-toggle-open");
 const closeMenuIcon = document.getElementById("menu-toggle-close");
 const mobileMenu = document.getElementById("mobile-menu");
 const headerOverlay = document.getElementById("header");
+const skyImage = document.getElementById("sky");
+const mountainsImage = document.getElementById("mountains");
+const heroData = document.getElementById("hero-data");
 
 openMenuIcon.addEventListener("click", () => {
   mobileMenu.classList.add("menu-opened");
