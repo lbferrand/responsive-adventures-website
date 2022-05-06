9ef8dcfe01c69632e800b37a17ce7322a927a860
lbferrand <lbferrand@gmail.com>
2022-05-05 20:09:05 -0400

    Correct some mistakes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const openMenuIcon = document.getElementById("menu-toggle-open");
 const closeMenuIcon = document.getElementById("menu-toggle-close");
 const mobileMenu = document.getElementById("mobile-menu");
 const headerOverlay = document.getElementById("header");
+const grassImage = document.getElementById("grassland");
 const skyImage = document.getElementById("sky");
 const mountainsImage = document.getElementById("mountains");
 const heroData = document.getElementById("hero-data");
@@ -23,6 +24,6 @@ window.addEventListener("scroll", () => {
   mountainsImage.style.transform =
     "translateY(" + -scrollPosition * 0.04 + "%)";
   skyImage.style.transform = "translateY(" + -scrollPosition * 0.01 + "%)";
-  heroData.style.transform = "translateY(" + -scrollPosition * 0.03 + "%)";
+  heroData.style.transform = "translateY(" + scrollPosition * 0.03 + "%)";
   heroData.style.opacity = 1 - scrollPosition * 0.002;
 });
